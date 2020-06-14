8904e3f4f4e23ef00d4085e3f0581228d5757fde
cyrilf <cyrilf7x@gmail.com>
2020-06-14 10:39:43 +0200

    Add Facebook link

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -2,9 +2,26 @@
   <footer class="bg-gray-100 bg-background-tertiary mt-10">
     <div class="max-w-6xl text-copy-primary text-center p-6 mx-auto flex">
       <div class="sm:flex-1"></div>
-      <div>Pensée Sauvage</div>
+      <div class="flex items-center">
+        <a
+          href="https://www.facebook.com/PenseeSauvageMusic"
+          target="_blank"
+          rel="noopener noreferrer"
+          ><g-image
+            src="~/../static/facebook.png"
+            width="25"
+            class="mr-2"
+            alt="facebook"
+        /></a>
+        <a
+          href="https://www.facebook.com/PenseeSauvageMusic"
+          target="_blank"
+          rel="noopener noreferrer"
+          >Pensée Sauvage</a
+        >
+      </div>
       <div class="flex-1 text-right flex justify-end items-center">
-        <a href="https://luseeds.com"
+        <a href="https://luseeds.com" target="_blank" rel="noopener noreferrer"
           ><img src="/favicon.ico" width="25" alt="favicon" class="inline mr-2"
         /></a>
         <a
